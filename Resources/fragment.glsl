37bb174df39455907a93cffc68c46bae1dacf129
YYH_YX <permanentnotforever@gmail.com>
2018-10-09 18:03:59 +0800

    add mix png

--- Resources/fragment.glsl
+++ Resources/fragment.glsl
@@ -1,8 +1,13 @@
 #version 330 core
 in vec3 ourColor;
+in vec2 texCoord;
+
 out vec4 FragColor;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
 }
